b30f42c98368ccce52a3e8d22341c59738634fba
Matthew Crotty <mjc2011@verizon.net>
2018-05-08 10:28:47 -0400

    Added basic algorithm TODO: use class methods

--- input_objects/intersection_control.py
+++ input_objects/intersection_control.py
@@ -10,5 +10,36 @@ Keys - Road Name
 Value - Cycle Time
 {'RoadNameN_S': 5, 'RoadNameE_W': 5}
 """
+def congestion(lane):
+    cars = 0
+    if len(lane) != 0:
+        for x in lane:
+            if x != 0:
+                cars+=1
+        return cars/len(lane)
 def run(time, intersection):
+    mRoads = intersection.roads
+    compare = {}
+    for x in mRoads:
+        sum = 0
+        for y in mRoads[x]['enter']:
+            for i in y.queue:
+                sum+=congestion(i)
+                #print(x, congestion(i))
+        for z in mRoads[x]['exit']:
+            for i in z.queue:
+                sum+=congestion(i)
+                #print(x, congestion(i))
+        compare[x] = sum
+    list = []
+    for x in mRoads:
+        list.append(x)
+    x, y = list[0], list[1]
+    if compare[x] > compare[y]:
+        intersection.cycle_times[x] += 1
+        intersection.cycle_times[y] -= 1
+    if compare[y] > compare[x]:
+        intersection.cycle_times[y] += 1
+        intersection.cycle_times[x] -= 1
+    print(intersection.cycle_times)
     return None
